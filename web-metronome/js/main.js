20e26a9050e3d6e69a27b06d02fe4a4746d45342
mserizawa <sasa.serizawa@gmail.com>
2018-01-03 23:14:13 +0900

    impl console

--- web-metronome/js/main.js
+++ web-metronome/js/main.js
@@ -1,16 +1,20 @@
+// load wav
 var ti = new Audio('sounds/ti.wav'),
-    co = new Audio('sounds/co.wav')
-    counter = 0;
+    co = new Audio('sounds/co.wav');
 
-var duration = 500,
-    width = window.innerWidth - 20,
-    height = window.innerHeight - 20;
+// init slider
+var tempoSlider = $('#tempo-slider').slider();
+
+// init d3
+var containerRect = d3.select('.js-donuts-container').node().getBoundingClientRect(),
+    width = containerRect.width,
+    height = width;
 
 var dataset = {
       lower: [0, 1],
       upper: [1, 0]
     },
-    radius = Math.min(width, height) / 3,
+    radius = Math.min(width, height) / 2.5,
     pie = d3.pie().sort(null);
 
 var arc = d3.arc()
@@ -30,20 +34,40 @@ var path = svg.selectAll('path')
     .attr('d', arc)
     .each(function (d) { this._current = d; });
 
-setInterval(function () {
-  co.pause();
-  co.currentTime = 0;
-  co.play();
+// attach button event
+var interval = null,
+    counter = 0;
+$('.js-toggle-click').on('click', function() {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+    counter = 0;
+  } else {
+    var duration = 60 / parseInt(tempoSlider.val()) * 1000;
+    duration = duration / 4 * parseInt($('select.rhythm-select.denominator').val());
+    interval = setInterval(function () {
+      if (counter % parseInt($('select.rhythm-select.numerator').val()) === 0) {
+        co.pause();
+        co.currentTime = 0;
+        co.play();
+      } else {
+        ti.pause();
+        ti.currentTime = 0;
+        ti.play();
+      }
+      counter++;
 
-  path.data(pie(dataset.lower))
-    .attr('d', arc)
-    .each(function (d) { this._current = d; });
+      path.data(pie(dataset.lower))
+        .attr('d', arc)
+        .each(function (d) { this._current = d; });
 
-  path = path.data(pie(dataset.upper));
-  path.transition().duration(duration).ease(d3.easeLinear)
-      .attrTween('d', function(a) {
-        var i = d3.interpolate(this._current, a);
-        this._current = i(0);
-        return function (t) { return arc(i(t)); };
-      });
-}, duration);
+      path = path.data(pie(dataset.upper));
+      path.transition().duration(duration).ease(d3.easeLinear)
+          .attrTween('d', function(a) {
+            var i = d3.interpolate(this._current, a);
+            this._current = i(0);
+            return function (t) { return arc(i(t)); };
+          });
+    }, duration);
+  }
+});
